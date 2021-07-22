b813a39aa15cde14771e1c31d389393643678299
Marc <marcsr03@gmail.com>
2021-07-22 19:51:42 +0200

    fixed some minor import issues

--- views/main/index.php
+++ views/main/index.php
@@ -1,52 +1,45 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?php echo constant('BASE_URL'); ?>node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="<?php echo constant('BASE_URL'); ?>node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="<?php echo constant('BASE_URL'); ?>node_modules/jquery/dist/jquery.min.js"></script>
-    <link rel="stylesheet" href="<?php echo constant('BASE_URL'); ?>assets/css/login.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo constant('BASE_URL'); ?>assets/images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo constant('BASE_URL'); ?>assets/images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo constant('BASE_URL'); ?>assets/images/favicon-16x16.png">
-    <link rel="manifest" href="<?php echo constant('BASE_URL'); ?>assets/images/site.webmanifest">
-    <link rel="mask-icon" href="<?php echo constant('BASE_URL'); ?>assets/images/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-    <title>PHP Employee Management V1</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo constant('BASE_URL'); ?>node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="<?php echo constant('BASE_URL'); ?>node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="<?php echo constant('BASE_URL'); ?>node_modules/jquery/dist/jquery.min.js"></script>
+        <link rel="stylesheet" href="<?php echo constant('BASE_URL'); ?>assets/css/login.css">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo constant('BASE_URL'); ?>assets/images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo constant('BASE_URL'); ?>assets/images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo constant('BASE_URL'); ?>assets/images/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo constant('BASE_URL'); ?>assets/images/site.webmanifest">
+        <link rel="mask-icon" href="<?php echo constant('BASE_URL'); ?>assets/images/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        <title>PHP Employee Management V1</title>
+    </head>
 
-<body>
-    <?php require_once "./views/header.php";?> 
-    <div class="d-flex login-container align-items-center justify-content-center">
-        <div class="login-form p-5 rounded">
-            <h3>Login</h3>
-            <form id="formLogin" action="<?php echo constant("BASE_URL")?>Login/registroEmployee" method="post">
-                <div class="mb-3">
-                    <input type="email" class="form-control" placeholder="Your Email *" value="" name="email" required />
-                </div>
-                <div class="mb-3">
-                    <input type="password" class="form-control" placeholder="Your Password *" name="password" value="" required />
-                </div>
-                <div class="mb-3">
-                    <input type="submit" class="btn btnSubmit" value="Login" />
-                </div>
-            </form>
-            <div class="text-danger"></div>
+    <body>
+        <?php require_once "./views/header.php";?> 
+        <div class="d-flex login-container align-items-center justify-content-center">
+            <div class="login-form p-5 rounded">
+                <h3>Login</h3>
+                <form id="formLogin" action="<?php echo constant("BASE_URL")?>Login/registroEmployee" method="post">
+                    <div class="mb-3">
+                        <input type="email" class="form-control" placeholder="Your Email *" value="" name="email" required />
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" placeholder="Your Password *" name="password" value="" required />
+                    </div>
+                    <div class="mb-3">
+                        <input type="submit" class="btn btnSubmit" value="Login" />
+                    </div>
+                </form>
+                <div class="text-danger"></div>
+            </div>
         </div>
-    </div>
-    <?php require_once "./views/footer.php";?> 
-</body>
+        <?php require_once "./views/footer.php";?> 
+    </body>
 <!-- <script src="./assets/js/index.js"></script> -->
 
 </html>
--- views/nuevo/index.php
+++ views/nuevo/index.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
+<?php 
+// require_once('./library/sessionHelper.php'); 
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo constant('BASE_URL'); ?>/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo constant('BASE_URL'); ?>/node_modules/jsgrid/dist/jsgrid.min.css" />
+    <link type="text/css" rel="stylesheet" href="<?php echo constant('BASE_URL'); ?>/node_modules/jsgrid/dist/jsgrid-theme.min.css" />
+    <link rel="stylesheet" href="<?php echo constant('BASE_URL'); ?>/assets/css/main.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo constant('BASE_URL'); ?>/assets/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo constant('BASE_URL'); ?>/assets/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo constant('BASE_URL'); ?>/assets/images/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo constant('BASE_URL'); ?>/assets/images/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo constant('BASE_URL'); ?>/assets/images/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title>Dashboard</title>
 </head>
-<body>
-    <h1>Archivo nuevo</h1>
+
+<body class="d-flex flex-column h-100">
+    <?php require_once "./views/header.php";?> 
+    <div class="container-fluid">
+
+        <main class="col-lg-10 p-2 mx-auto gy-2">
+            <div>
+                <h2>Employees</h2>
+            </div>
+            <div id="jsGrid"></div>
+        </main>
+    </div>
+
+
+    <?php require_once "./views/footer.php";?> 
 </body>
+
+<script type="text/javascript" src="<?php echo constant('BASE_URL'); ?>node_modules/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo constant('BASE_URL'); ?>node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="<?php echo constant('BASE_URL'); ?>node_modules/jsgrid/dist/jsgrid.min.js"></script>
+<script type="text/javascript" src="<?php echo constant('BASE_URL'); ?>assets/js/jsgrid.js"></script>
+
 </html>
