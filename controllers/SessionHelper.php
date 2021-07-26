59b44ea8cb751fb3349588cf7cd9de1be73e77aa
Marc <marcsr03@gmail.com>
2021-07-26 16:54:47 +0200

    everything fixed in employee and login hopefully. Error testing is required

--- controllers/SessionHelper.php
+++ controllers/SessionHelper.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+    // session_start();
     require_once('./config/constants.php');  
 
     class SessionHelper extends Controller{
--- models/LoginModel.php
+++ models/LoginModel.php
@@ -1,53 +1,55 @@
 <?php
 
-class LoginModel extends Model{
-  public function __construct(){
-      parent:: __construct();
-      
+  class LoginModel extends Model{
+    public function __construct(){
+        parent:: __construct();
+        
 
-  }
-  public function getUsers(){
+    }
+    public function getUsers(){
       
-      $query=$this->db->connect()->query(GETALL_USERS);
-
-   
-        while ($row = $query->fetch()) {
-            $dataQuestions[] = $row;
-            
-        }
-        return $dataQuestions;
-    
-    // $conn->close();
-    }
-  
-  public function getUserData($use, $users)
-    {
-            foreach($users as $user){
-             if($user['us_name']==$use){
-               return $user['us_name'];              
-              }
-            };  
-                  
-    }
+      try {
+          $query=$this->db->connect()->query(GETALL_USERS);
+          while ($row = $query->fetch()) {
+              $users[] = $row;
+          }
+          return $users;
           
-
-  public function checkUserPass($password, $users,$use){
-   //print_r ($users);
-   //session_start();
-    foreach($users as $user){
-    
-        if(password_verify($password, $user['us_password'])&&$user["us_name"]==$use){
-                http_response_code(200);
-                
-                $_SESSION['userId'] = $user["us_name"];
-                $_SESSION['creatorId'] = $user["us_id"];
-                $_SESSION['time'] = time();                  
-        }else if(!password_verify($password, $user['us_password'])){ 
-          $_SESSION['wrongPassword']="Wrong password";
+        }catch(\PDOException $e){
+          $_SESSION['error'] = "Error from GET ALL USERS is:\n ".$e;
+          return null;
         }
+    
       }
-  }  
-}
+    
+    public function getUserData($use, $users)
+      {
+        foreach($users as $user){
+        if($user['us_name']==$use){
+          return $user['us_name'];              
+          }
+        }
+                    
+      }
+            
+
+    public function checkUserPass($password, $users,$use){
+    //print_r ($users);
+    //session_start();
+      foreach($users as $user){
+      
+          if(password_verify($password, $user['us_password'])&&$user["us_name"]==$use){
+                  http_response_code(200);
+                  
+                  $_SESSION['userId'] = $user["us_name"];
+                  $_SESSION['creatorId'] = $user["us_id"];
+                  $_SESSION['time'] = time();                  
+          }else if(!password_verify($password, $user['us_password'])){ 
+            $_SESSION['wrongPassword']="Wrong password";
+          }
+        }
+    }  
+  }
   //   $pwHash = array_reduce($users, function ($carry, $user) use ($email) {
   //     foreach($users as $user){
       
