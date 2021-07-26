90f7ae1db6a483e9fce99d355fd0138ea944e7ad
Marc <marcsr03@gmail.com>
2021-07-26 15:58:47 +0200

    error handling for dashboard code completed, but needs further testing

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -11,21 +11,39 @@
         }
 
         public function getAllEmployees(){
-            echo json_encode($this->model->getAllEmployees());
+
+            $result = $this->model->getAllEmployees();
+            if($result === null){
+
+                return false;
+
+            } else if(empty($result)){
+                
+                $_SESSION['error'] = "Error from GET ALL EMPLOYEES is: entry does not exist";
+                return false;
+
+            }else{
+                echo json_encode($result);
+            }
         }
 
         public function dataEmployee($id){
             $employeeId=(int)$id;
-            session_start();
-            $_SESSION["employeeData"]=$this->model->getEmployeeData($employeeId);
-            header("Location:".BASE_URL."Employee/renderEmployee");
-        }
+            $_SESSION["employeeData"] = $this->model->getEmployeeData($employeeId);
 
-        public function getEmployeeById($employee_no){
-            
-            session_start();
-            $_SESSION['employee'] = $this->model->getEmployeeById($employee_no);
+            if($_SESSION["employeeData"] === null){
 
+                return false;
+
+            } else if(empty($_SESSION["employeeData"])){
+                
+                $_SESSION['error'] = "Error from GET_EMPLOYEE_BY_ID is: entry does not exist";
+                return false;
+
+            }else{
+
+                header("Location:".BASE_URL."Employee/renderEmployee");
+            }
         }
 
         public function insertEmployee(){
@@ -43,7 +61,7 @@
                 $emp_postalCode     =   $_POST['emp_postalCode'];
                 $emp_phoneNum       =   $_POST['emp_phoneNum'];
                 
-                $this->model->insertEmployee([
+                if($this->model->insertEmployee([
                     'us_id'             => $us_id,
                     'emp_name'          => $emp_name,
                     'emp_lastName'      => $emp_lastName,
@@ -54,14 +72,18 @@
                     'emp_state'         => $emp_state,
                     'emp_postalCode'    => $emp_postalCode,
                     'emp_phoneNum'      => $emp_phoneNum,
-                ]);
+                ]) === null){
+                    return false;
+                }
 
             }
         }
 
         public function deleteEmployee($employee_no){
             
-            $this->model->deleteEmployee($employee_no);
+            if($this->model->deleteEmployee($employee_no) === null){
+                return false;
+            }
 
         }
     }
--- models/DashboardModel.php
+++ models/DashboardModel.php
@@ -9,44 +9,72 @@
 
     public function getAllEmployees(){
 
-      $query = $this->db->connect()->query(GETALL_EMPLOYEES);
-      $result = $query->fetchAll(\PDO::FETCH_ASSOC);
-      return $result;
-      // $conn->close();
+        try {
+
+          $query = $this->db->connect()->query(GETALL_EMPLOYEES);
+          $result = $query->fetchAll(\PDO::FETCH_ASSOC);
+          return $result;
+
+        }catch(\PDOException $e){
+          $_SESSION['error'] = "Error from GET ALL EMPLOYEES is:\n ".$e;
+          return null;
+        }
+
       }
       
       public function getEmployeeData($id){
 
-        $query = $this->db->connect()->prepare(GET_EMPLOYEE_BY_ID);
-        $query->execute(["employee_no" => $id]);
-        $result = $query->fetchAll(\PDO::FETCH_ASSOC);
-        return $result ;
+        try{
+
+          $query = $this->db->connect()->prepare(GET_EMPLOYEE_BY_ID);
+          $query->execute(["employee_no" => $id]);
+          $result = $query->fetchAll(\PDO::FETCH_ASSOC);
+          return $result ;
+
+        }catch(\PDOException $e){
+          $_SESSION['error'] = "Error from GET EMPLOYEE BY ID is:\n ".$e;
+          return null;
+        }
         
       }
     
 
       public function insertEmployee($data){
         
-        $query = $this->db->connect()->prepare(INSERT_EMPLOYEE_DASHBOARD);
-        $query->execute([
-          'us_id'             => $data['us_id'],
-          'emp_name'          => $data['emp_name'], 
-          'emp_lastName'      => $data['emp_lastName'], 
-          'emp_email'         => $data['emp_email'], 
-          'emp_age'           => $data['emp_age'], 
-          'emp_streetAddress' => $data['emp_streetAddress'], 
-          'emp_city'          => $data['emp_city'], 
-          'emp_state'         => $data['emp_state'], 
-          'emp_postalCode'    => $data['emp_postalCode'], 
-          'emp_phoneNum'      => $data['emp_phoneNum']
-        ]);
+        try{
+
+          $query = $this->db->connect()->prepare(INSERT_EMPLOYEE_DASHBOARD);
+          $query->execute([
+            'us_id'             => $data['us_id'],
+            'emp_name'          => $data['emp_name'], 
+            'emp_lastName'      => $data['emp_lastName'], 
+            'emp_email'         => $data['emp_email'], 
+            'emp_age'           => $data['emp_age'], 
+            'emp_streetAddress' => $data['emp_streetAddress'], 
+            'emp_city'          => $data['emp_city'], 
+            'emp_state'         => $data['emp_state'], 
+            'emp_postalCode'    => $data['emp_postalCode'], 
+            'emp_phoneNum'      => $data['emp_phoneNum']
+          ]);
+
+        }catch(\PDOException $e){
+          $_SESSION['error'] = "Error from INSERT EMPLOYEE DASHBOARD is:\n ".$e;
+          return null;
+        }
 
       }
 
       public function deleteEmployee($employee_no){
 
-        $query = $this->db->connect()->prepare(DELETE_EMPLOYEE);
-        $query->execute(['employee_no' => $employee_no]);
+        try{
+
+          $query = $this->db->connect()->prepare(DELETE_EMPLOYEE);
+          $query->execute(['employee_no' => $employee_no]);
+
+        }catch(\PDOException $e){
+          $_SESSION['error'] = "Error from DELETE EMPLOYEE  is:\n ".$e;
+          return null;
+        }
         
       }
 
